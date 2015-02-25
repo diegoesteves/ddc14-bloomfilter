c19f42ef4ffc176609a40a1df51b957378d34dc3
Diego Esteves <diegoesteves@gmail.com>
2015-02-25 21:06:49 +0100

    fixing error

--- tempdeletar.js
+++ tempdeletar.js
@@ -12,4 +12,4 @@ fs.readFileSync('./texto.txt').toString().split('\n').forEach(function (line) {j
     if (check != -1) {for (var i = 0; i < line.length; i++){if ((line.substring(i,i+1) == "\"") && (line.substring(i-1,i) != "\\")) {indexs[j]=i; j++;}}
         if (j==4){prefix=line.toString().substring(1,indexs[0]);sufx=line.toString().substring(indexs[3],line.length);t1=line.toString().substring(indexs[0],indexs[1]);t2=line.toString().substring(indexs[2],indexs[3]+1);newline = prefix + t2 + ", " + t1 + sufx;
             fs.appendFileSync('./output.txt', newline.toString() + "\n");}}
-    else {fs.appendFileSync('./output.txt', line.toString() + "\n");}}) process.exit(0);
+    else {fs.appendFileSync('./output.txt', line.toString() + "\n");}}); process.exit(0);
